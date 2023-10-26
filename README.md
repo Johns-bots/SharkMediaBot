bd94d195e93919ab786f7989066f5ca75409556e
John-Fries-J <w.bababooie@gmail.com>
2023-10-26 03:35:24 +0100

    Fix stuff

--- README.md
+++ README.md
@@ -1,41 +1,5 @@
 <h1 align="center">
-Suggestion Bot
+Shark Bot
   <br>
 </h1>
 
-
-## Features
-
-`!suggest`: Suggests a suggestion <br>
-`!accept [messageID]` : Accepts a suggestion (Do not spam) <br>
-`!decline [messageID]` : Declines a suggestion (Do not spam) <br>
-
-## Get Started
-
-# Create a config.json file
-
-
-Add in the config.json file as it shows in image below  
-
-<h1 align="center">
-<a href="https://github.com/john-fries-j"><img src="https://cdn.discordapp.com/attachments/1118868994078818385/1164759063406587905/image.png">
-</a>
-</h1>
-
-
-
-# Create .env file
-
-Add in the .env file the following line
-
-TOKEN=
-
-Run npm i in terminal
-
-run node index.js in terminal
-test your commands
-any errors make a pull request.
-
-## To-Do's
-Make bot Application Commands
-Add modals at some point.
--- commands/Announcements/announce.js
+++ commands/Announcements/announce.js
@@ -5,18 +5,19 @@ const Discord = require('discord.js');
 module.exports = class extends Command {
     constructor(...args) {
       super(...args, {
-        name: 'suggest',
-        aliases: ["s", "sug","suggest"],
-        description: `Suggest a suggestion!`,
-        category: 'Suggestions',
+        name: 'announce',
+        aliases: ["a", "ann"],
+        description: `Announcements`,
+        category: 'announce',
         cooldown: 10,
+        userPermission:['ADMINISTRATOR'],
         botPermission: ["ADD_REACTIONS"]
       });
     }
 
     async run(message, args) {
 
-      console.log(`Discord Suggestion Ran. From user ${message.author.tag}`)
+      console.log(`Discord announcement Ran. From user ${message.author.tag}`)
       const channelId = `${config.logChannelID}`; 
       const logChannel = message.guild.channels.cache.get(channelId);
 
@@ -27,43 +28,37 @@ module.exports = class extends Command {
       } else channel = await message.guild.channels.cache.find(c => c.name == "suggestions" && c.type == "text");
 
       if(!channel){
-      return message.channel.send(`${message.client.emoji.fail} | I could not find the suggestion channel in the current guild.`)
+      return message.channel.send(`${message.client.emoji.fail} | I could not find the announcement channel in the current guild.`)
       };
 
-      const suggestion = args.slice(0).join(" ")
-      if(!suggestion){
-        return message.channel.send(`${message.client.emoji.fail} | You need to type !suggest [Your suggestion]`)
-      };
+      // const suggestion = args.slice(0).join(" ")
+      // if(!suggestion){
+      //   return message.channel.send(`${message.client.emoji.fail} | You need to type !announce`)
+      // };
 
       const embed = new Discord.MessageEmbed()
-      .setThumbnail(message.author.avatarURL())
-      .setDescription(`**Submitter**\n ${message.author.tag} \n\n**Suggestion**\n ${suggestion}`)
-      .setFooter(`Suggested by ${message.author.id}`)
-      .setTimestamp()
+      .setTitle('Hello Performance Lexus')
+      .setDescription(`Let's book our next shoot day! Please select a day and time on our Google form. Once we receive your request, we'll confirm with our team and get back to you as soon as possible!`)
       .setColor(message.client.color.blue)
+      .setFooter('Shark Media Team')
     
       channel.send(embed)
-      .then((s)=>{
-
-      s.react(`✅`)
-      s.react(`❌`)
 
       const log = new Discord.MessageEmbed()
       .setThumbnail(message.author.avatarURL())
       .setAuthor(`${message.author.tag}`)
-      .setDescription(`Suggestion sent by <@${message.author.id}> in <#${config.suggestion_channel_id}>\n ${suggestion}`)
+      .setDescription(`Announcement sent by <@${message.author.id}> in <#${config.suggestion_channel_id}>`)
       .setFooter(`Author: ${message.author.id} | Message ID: ${message.id}`)
       .setTimestamp()
       .setColor(message.client.color.blue)
       logChannel.send(log)
 
-      })
       .catch(()=>{
-        return message.reply(`${message.client.emoji.fail} | Could not send a message to the suggestion Channel.`)
+        return message.reply(`${message.client.emoji.fail} | Could not send a message to the announcement Channel.`)
       });
       
       message.delete();
-      return message.channel.send(`${message.client.emoji.success} | Successfuly sent your suggestion to ${channel}`)
+      return message.channel.send(`${message.client.emoji.success} | Successfuly sent your announcement to ${channel}`)
       }
 };
 
